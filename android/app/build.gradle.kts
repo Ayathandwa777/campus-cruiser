8319c656d78d338098932f5ffe0ce221fd255392
Ayathandwa777 <Ayatthandwa@gmail.com>
2025-05-01 18:18:02 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.campus_cruiser"
+    namespace = "com.example.cam"
     compileSdk = 34
     ndkVersion = "27.0.12077973"
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.campus_cruiser"
+        applicationId = "com.youruni.shuttletracker"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
